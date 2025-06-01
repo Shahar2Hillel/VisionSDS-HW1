3b6c91a948240535d803e6fa10b8cec3afe5d7ea
its-Raz <117819056+its-Raz@users.noreply.github.com>
2025-06-01 15:15:37 +0300

    Update video.py

--- video.py
+++ video.py
@@ -1,7 +1,7 @@
 import sys
 import cv2
 from ultralytics import YOLO
-model = YOLO("best.pt")
+model = YOLO("model.pt")
 video_path = "/datashare/HW1/ood_video_data/surg_1.mp4"
 output_path = "video_output.avi"
 # --- Open video ---
